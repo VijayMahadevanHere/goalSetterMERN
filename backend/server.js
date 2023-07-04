c3fc7a411b62d3918973cd6b68a380ada8e8213c
Vijaymahadevan97 <vijaymahadevan97@gmail.com>
2023-07-04 17:09:27 +0530

    finalcommit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path=require('path')
 const express=require("express")
 const app=express()
 const colors=require('colors')
@@ -12,10 +13,17 @@ connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-
 app.use('/api/goals',goalRoutes)
 app.use('/api/users',userRoutes)
 
+//Serve FrontEnd
+if(process.env.NODE_ENV==='production'){
+app.use(express.static(path.join(__dirname,'../frontend/build')))
+app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'../','frontend','build','index.html')))
+}else{
+app.get('/',(req,res)=>res.send('Please set to production'))
+}
+   
 app.use(errorHandler) 
 
       
